14ee2f1543a16348eca054a2aabb11ac99ee85f4
Topolechek <Topolev-vlad@mail.ru>
2022-12-31 13:12:51 +0300

    decimal_calculate_1.1

--- tgbot/services/parser.py
+++ tgbot/services/parser.py
@@ -3,7 +3,9 @@
 def calcuate(mid_price, quality, price_now, how_much):
     all_price_now = mid_price * quality
     new_mid_price = ((price_now * how_much) + all_price_now) / (how_much + quality)
-    return round(all_price_now), round(new_mid_price)
+    return round(all_price_now, 2), round(new_mid_price, 2)
 
 def inspect(msg):
+    msg = msg.replace(',','1')
+    msg = msg.replace('.', '1')
     return msg.isdigit()
